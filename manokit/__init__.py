03970bec57821588a0c8e6a5315a65110ea6a4d6
nickythelion Nikolai Beloguzov <nickythelionfurry@gmail.com>
2023-09-13 21:17:56 +1000

    Added license disclaimer

--- manokit/__init__.py
+++ manokit/__init__.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2023  Nikolai Beloguzov a/k/a nickythelion
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from email.mime.base import MIMEBase
 from pathlib import Path
 import re
